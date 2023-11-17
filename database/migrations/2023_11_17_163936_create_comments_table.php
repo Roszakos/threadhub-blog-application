79c586d0f3bb32653fbbe9c44a2bedf0b3a1ef36
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-17 17:44:06 +0100

    Create database migrations

---
+++ database/migrations/2023_11_17_163936_create_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor('post_id');
+            $table->foreignIdFor(User::class);
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
