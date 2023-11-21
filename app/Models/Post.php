494b23e0348d69f58ebdd4bc54f190a66674feac
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-21 21:56:10 +0100

    Update Post.php
    Implement creating a slug usign Sluggable package

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use App\Models\User;
 use App\Models\Comment;
 use App\Models\PostSection;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -13,12 +15,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Post extends Model
 {
     use HasFactory;
+    use HasSlug;
 
     protected $fillable = [
         'user_id',
         'title',
         'slug'
     ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(100);
+    }
 
     public function user(): BelongsTo
     {
