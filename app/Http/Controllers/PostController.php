7ac2af11e17bcaff0e366bc20e8ba6577e7385f9
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-21 20:47:26 +0100

    Update PostController.php
    Implement storing new post in database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\PostSection;
 use App\Http\Requests\StorePostRequest;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -14,17 +16,33 @@ class PostController extends Controller
 
     public function store(StorePostRequest $request)
     {
-        $data = $request->validated();
-        $data['slug'] = $data['title'];
-
-        $post = Post::create($data);
-
-        // for ($i = 0; $i < count($data['subtitle']); $i++) {
-        //     if ($data['subtitle'][$i] && $data['content'][$i]) {
-        //         $this->createSection($data['subtitle'][$i], $data['content'][$i], $post->id);
-        //     }
-        // }
+        if ($data = $request->validated()) {
+            $data['slug'] = $data['title'];
+            $post = Post::create($data);
+            for ($i = 0; $i < count($data['subtitle']); $i++) {
+                if ($data['subtitle'][$i] && $data['content'][$i]) {
+                    $this->createSection([
+                        'title' => $data['subtitle'][$i],
+                        'content' => $data['content'][$i],
+                        'post_id' => $post->id
+                    ]);
+                }
+            }
+        } else {
+            return back();
+        }
 
         return view('post.create');
+    }
+
+    private function createSection($data)
+    {
+        $validator = Validator::make($data, [
+            'title' => ['required', 'string', 'max:200'],
+            'content' => ['required', 'string', 'max:10000'],
+            'post_id' => ['required', 'numeric', 'exists:Posts,id']
+        ]);
+
+        return PostSection::create($validator->validated());
     }
 }
