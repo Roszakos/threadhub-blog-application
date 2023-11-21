3a73b29cd4d9b63550ed802b0635081b369256ca
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-21 16:21:03 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <div class="flex justify-between">
+                        <div class="text-lg">
+                            {{ __('Your posts') }}
+                        </div>
+                        <a href="{{ route('post.create') }}"
+                            class="py-2 px-3 bg-sky-400 ring-1 ring-black cursor-pointer rounded-sm
+                            hover:bg-sky-500 hover:ring-2">
+                            {{ __('+ Add new post') }}
+                        </a>
+                    </div>
+                    <div class="grid gap-4 grid-cols-2 mt-5">
+                        <div class="bg-slate-200 py-20 col-span-1">
+                            {{-- {{ __('xd') }} --}}
+                        </div>
+                        <div class="bg-slate-200 py-20 col-span-1">
+                            {{-- {{ __('xd') }} --}}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
