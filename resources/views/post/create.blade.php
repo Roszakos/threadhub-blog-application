75c2ce202507be4454762904a14850c4c92a8927
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-21 16:29:17 +0100

    Create create.blade.php
    Create view for creating new post

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create new post') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <x-post.new-post-form />
+            </div>
+        </div>
+    </div>
+</x-app-layout>
