f02a294da5c2c52dd35931977a16e5265b0e3bda
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-21 20:46:09 +0100

    Update StorePostRequest.php
    Declare rules and error messages for validation of new post

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -4,14 +4,21 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StorePost extends FormRequest
+class StorePostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'user_id' => $this->user()->id
+        ]);
     }
 
     /**
@@ -22,7 +29,22 @@ class StorePost extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'user_id' => 'exists:users,id',
+            'title' => 'required|string|max:200',
+            'subtitle' => 'required|array',
+            'content' => 'required|array',
+            'subtitle.*' => 'string|required|max:200',
+            'content.*' => 'string|required|max:10000',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'This field is required.',
+            'string' => 'This field must be a string.',
+            'max:200' => 'This field cannot be longer than 200 characters.',
+            'max:10000' => 'The content of 1 section cannot be longer than 10000 characters'
         ];
     }
 }
