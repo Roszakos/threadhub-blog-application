a32f6aef0be875ce98b9c931579f4384297c3202
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-23 23:02:01 +0100

    Create edit.blade.php
    View for editing a post

---
+++ resources/views/post/edit.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit your post') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <x-post.post-form :items="array_fill(0, count($postSectionsSubtitle), 1)" :postAction="route('post.update', $post->slug)" :title="$post->title" :subtitles="$postSectionsSubtitle"
+                    :contents="$postSectionsContent" />
+            </div>
+        </div>
+    </div>
+</x-app-layout>
