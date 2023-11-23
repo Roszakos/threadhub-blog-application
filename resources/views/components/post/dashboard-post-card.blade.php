8b13956ca36745c5b96f2a0737e66b7f6f1fa3a9
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-23 23:01:42 +0100

    Update dashboard-post-card.blade.php

--- resources/views/components/post/dashboard-post-card.blade.php
+++ resources/views/components/post/dashboard-post-card.blade.php
@@ -1,10 +1,19 @@
 @props(['title', 'created', 'updated', 'slug'])
 
-<a href="{{ route('post.view', $slug) }}" class="relative bg-slate-200 col-span-1 h-[10rem]">
-    <div class="font-semibold text-xl mt-3 ml-5">
-        {{ $title }}
+<div x-data="postCard" @@click="redirect('{{ route('post.view', $slug) }}')"
+    class="relative bg-slate-200 col-span-1 h-[10rem] cursor-pointer hover:bg-slate-300">
+    <div class="flex justify-between w-[94%] mt-3 m-auto">
+        <div class="font-semibold text-xl">
+            {{ $title }}
+        </div>
+        <div @@click.stop="redirect('{{ route('post.edit', $slug) }}')"
+            href="{{ route('post.edit', $slug) }}"
+            class="py-1 px-5 text-md bg-slate-400 ring-1 ring-black rounded-sm cursor-pointer
+        hover:bg-slate-500 hover:ring-2">
+            Edit
+        </div>
     </div>
-    <div class="absolute bottom-3 left-[1.25rem] text-sm flex justify-between w-[30%] flex-col">
+    <div class="absolute bottom-3 left-[3%] text-sm flex justify-between w-[30%] flex-col">
         <div class="flex w-full justify-between">
             <div>
                 {{ __('Created: ') }}
@@ -15,11 +24,20 @@
         </div>
         <div class="flex w-full justify-between">
             <div>
-                {{ __('Last update: ') }}
+                {{ __('Last Edit: ') }}
             </div>
             <div>
                 {{ date('m-d H:i', strtotime($updated)) }}
             </div>
         </div>
     </div>
-</a>
+</div>
+<script>
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('postCard', () => ({
+            redirect(URL) {
+                window.location.href = URL
+            }
+        }))
+    })
+</script>
