7a6f5ba1bf524ac91fed44eb9cf3f04ba8a85ac8
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-24 12:40:19 +0100

    Update edit.blade.php

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <x-post.post-form :items="array_fill(0, count($postSectionsSubtitle), 1)" :postAction="route('post.update', $post->slug)" :title="$post->title" :subtitles="$postSectionsSubtitle"
-                    :contents="$postSectionsContent" />
+                    :contents="$postSectionsContent" action="edit" />
             </div>
         </div>
     </div>
