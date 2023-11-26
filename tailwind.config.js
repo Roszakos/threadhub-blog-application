4256917666c84a09fc4b034c5a1481256edc569e
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-26 21:59:43 +0100

    Update tailwind.config.js
    Create custom keyframes and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,59 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            keyframes: {
+                'expand-to-full': {
+                    'from': {
+                        height: 0
+                    },
+                    'to': {
+                        height: '100%',
+                        borderTopRightRadius: '1rem',
+                        borderTopLeftRadius: '1rem',
+                        
+                    }
+                },
+                'shrink-from-full': {
+                    'from': {
+                        height: '100%',
+                        borderTopRightRadius: '1rem',
+                        borderTopLeftRadius: '1rem',
+                    },
+                    'to': {
+                        borderTopRightRadius: '0rem',
+                        borderTopLeftRadius: '0rem',
+                        height: 0
+                    }
+                },
+                'opacity-to-100': {
+                    '0%': {
+                        opacity: 0,
+                    },
+                    '50%' : {
+                        opacity: 0,
+                    },
+                    '100%': {
+                        opacity: 100,
+                    }
+                },
+                'opacity-to-0': {
+                    '0%': {
+                        opacity: 100,
+                    },
+                    '50%' : {
+                        opacity: 0,
+                    },
+                    '100%': {
+                        opacity: 0,
+                    }
+                }
+            },
+            animation: {
+                'expand-to-full': "expand-to-full 0.4s ease-in-out both",
+                'shrink-from-full': "shrink-from-full 0.4s ease-in-out both",
+                'opacity-to-100': "opacity-to-100 0.4s ease-in-out both",
+                'opacity-to-0': "opacity-to-0 0.4s ease-in-out both"
+            }
         },
     },
 
