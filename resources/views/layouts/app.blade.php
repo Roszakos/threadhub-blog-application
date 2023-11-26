fd0de88ee204066bec8541a2de0bc2412f20f2c4
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-26 21:59:49 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 py-5">
+    <div class="min-h-screen bg-gray-100 pb-5">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
