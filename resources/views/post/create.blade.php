85584a40c7330edd31bd6b288555856f4e0a5c5d
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-26 14:16:47 +0100

    Update create.blade.php

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create new post') }}
+            {{ __('Create new article') }}
         </h2>
     </x-slot>
 
