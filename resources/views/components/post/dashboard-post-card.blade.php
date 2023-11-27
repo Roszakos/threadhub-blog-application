03c5e677e281837e573d11559c661d2ab2a331b9
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-27 18:31:34 +0100

    Update dashboard-post-card.blade.php
    Implement showing article image in user's dashboard

--- resources/views/components/post/dashboard-post-card.blade.php
+++ resources/views/components/post/dashboard-post-card.blade.php
@@ -1,37 +1,40 @@
-@props(['title', 'created', 'updated', 'slug'])
+@props(['post'])
 
-<div x-data="postCard" @@click="redirect('{{ route('post.view', $slug) }}')"
-    class="relative bg-slate-200 col-span-1 h-[10rem] cursor-pointer hover:bg-slate-300">
-    <div class="flex justify-between w-[94%] mt-3 m-auto">
-        <div class="font-semibold text-xl">
-            {{ $title }}
-        </div>
-    </div>
-    <div class="absolute bottom-3 left-[3%] text-sm flex justify-between w-[94%]">
-        <div class="flex flex-col w-[35%]">
-            <div class="flex w-full justify-between">
-                <div>
-                    {{ __('Created: ') }}
-                </div>
-                <div>
-                    {{ date('m-d H:i', strtotime($created)) }}
-                </div>
-            </div>
-            <div class="flex w-full justify-between">
-                <div>
-                    {{ __('Last Edit: ') }}
-                </div>
-                <div>
-                    {{ date('m-d H:i', strtotime($updated)) }}
-                </div>
+<div x-data="postCard" @@click="redirect('{{ route('post.view', $post->slug) }}')"
+    class="border-2 relative bg-slate-200 col-span-1 h-[10rem] cursor-pointer hover:bg-slate-300 bg-center bg-cover"
+    style="background-image: url({{ asset($post->image) }})">
+    <div class="absolute top-0 w-full h-full bg-white/70">
+        <div class="flex justify-between w-[94%] mt-3 m-auto">
+            <div class="font-semibold text-xl">
+                {{ $post->title }}
             </div>
         </div>
-        <div class="flex gap-2 h-9">
-            <div class="!bg-gray-400 hover:!bg-gray-500 text-black inline-flex items-center px-4 py-2 border border-gray-300 rounded-md font-semibold text-xs  uppercase tracking-widest shadow-sm  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
-                @@click.stop.prevent="redirect('{{ route('post.edit', $slug) }}')">
-                {{ __('Edit') }}
+        <div class="absolute bottom-3 left-[3%] text-sm flex justify-between w-[94%]">
+            <div class="flex flex-col w-[35%]">
+                <div class="flex w-full justify-between">
+                    <div>
+                        {{ __('Created: ') }}
+                    </div>
+                    <div>
+                        {{ date('m-d H:i', strtotime($post->created_at)) }}
+                    </div>
+                </div>
+                <div class="flex w-full justify-between">
+                    <div>
+                        {{ __('Last Edit: ') }}
+                    </div>
+                    <div>
+                        {{ date('m-d H:i', strtotime($post->updated_at)) }}
+                    </div>
+                </div>
             </div>
-            <x-post.delete-post-form :slug="$slug" :name="__('delete-') . $slug" @@click.stop />
+            <div class="flex gap-2 h-9">
+                <div class="!bg-gray-400 hover:!bg-gray-500 text-black inline-flex items-center px-4 py-2 border border-gray-300 rounded-md font-semibold text-xs  uppercase tracking-widest shadow-sm  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
+                    @@click.stop.prevent="redirect('{{ route('post.edit', $post->slug) }}')">
+                    {{ __('Edit') }}
+                </div>
+                <x-post.delete-post-form :slug="$post->slug" :name="__('delete-') . $post->slug" @@click.stop />
+            </div>
         </div>
     </div>
 </div>
