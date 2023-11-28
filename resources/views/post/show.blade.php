d92a334c4e9faa81ee2f471b21b49230c4f78e7e
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-28 17:37:10 +0100

    Implement showing votes on post view

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-
     <div class="w-full">
         <div class="text-3xl font-bold text-center pt-3 pb-12 leading-7 tracking-wider h-[20rem] bg-gray-200 bg-cover bg-center relative"
             style="background-image: linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url({{ asset($post->image) }})">
@@ -7,9 +6,15 @@
                 <div class="max-w-6xl mx-auto text-left text-white">
                     {{ $post->title }}
                 </div>
-                <div class="max-w-6xl mx-auto text-left text-white items-end mt-2 flex gap-3 justify-end font-normal">
-                    <div class="text-xl">{{ $post->author }}</div>
-                    <div class="text-xl">{{ date('m-d H:i', strtotime($post->created_at)) }}</div>
+
+                <div class="flex items-end justify-between max-w-6xl mx-auto pt-3">
+                    <x-post.votes :upvotes="$upvotes" :downvotes="$downvotes" :vote="$vote" :postId="$post->id"/>
+
+                    <div
+                        class="text-left text-white items-end mt-2 flex gap-3 justify-end font-normal">
+                        <div class="text-xl">{{ $post->author }}</div>
+                        <div class="text-xl">{{ date('m-d H:i', strtotime($post->created_at)) }}</div>
+                    </div>
                 </div>
             </div>
         </div>
