50a936ae000deaf701883d6e031e0f881362e116
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-12-02 13:15:15 +0100

    Update CommentController.php
    Implement storing, updating and deleting comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Http\Requests\CommentRequest;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
-    //
+    public function store(CommentRequest $request)
+    {
+        
+        $validator = Validator::make($request->all(), [
+            'post_id' => [
+                'required',
+                'exists:Posts,id'
+            ],
+            'user_id' => [
+                'nullable',
+                'exists:Users,id'
+            ],
+            'content' => [
+                'required',
+                'string',
+                'max:500'
+            ]
+        ]);
+
+        $validator->sometimes('author', 'required|string|max:100', function($request) {
+            return !$request->user_id;
+        });
+
+        $data = $validator->validated();
+
+        return (Comment::create($data));
+    }
+
+    public function update(Comment $comment, Request $request)
+    {
+        $data = $request->validate([
+            'content' => [
+                'required',
+                'string',
+                'max:500'
+            ]
+        ]);
+
+        $comment->content = $data['content'];
+        $comment->save();
+
+        return response('success', 200);
+    }
+
+    public function destroy(Comment $comment)
+    {
+        if ($comment->delete()) {
+            return response('success', 200);
+        } else {
+            return response('error', 500);
+        }
+    }
 }
