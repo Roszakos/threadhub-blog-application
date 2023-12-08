76a93aeec847424e5d08eb6a216b532fc8b2b78a
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-12-08 19:24:22 +0100

    Display user's comments on his profile page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function show(User $user)
+    public function show(User $user, Request $request)
     {
-        // $posts = DB::table('posts')
-        //             ->join('votes', 'votes.post_id', '=', 'posts.id')
-        //             ->select('title', 'image', 'slug', 'created_at');
-        $posts = $user->posts;
-        $comments = $user->comments;
-        return view('user-profile', ['user' => $user, 'posts' => $posts, 'comments' => $comments]);
+        if ($request->user() && $user->id === $request->user()->id) {
+            $accountOwner = true;
+        } else {
+            $accountOwner = false;
+        }
+
+        $posts = $user->posts->sortByDesc('created_at');
+        $comments = $user->comments->sortByDesc('created_at')->map(function ($comment) use ($user) {
+            $dt = Carbon::create($comment->created_at);
+            $comment->{'posted'} = $dt->diffForHumans();
+            $comment->{'postSlug'} = DB::table('posts')
+                                        ->select('slug')
+                                        ->where('id', '=', $comment->post_id)
+                                        ->value('slug');
+            $comment->author = $user->nickname;
+            return $comment;
+        })->filter(function ($comment) {
+            return $comment->postSlug;
+        });
+
+        return view('user-profile', ['user' => $user, 'posts' => $posts, 'comments' => $comments, 'accountOwner' => $accountOwner]);
     }
 }
--- resources/views/user-profile.blade.php
+++ resources/views/user-profile.blade.php
@@ -21,51 +21,81 @@
                             </div>
                         </div>
                     </div>
-                    <div>
-                        {{ __('Joined us ') . date('m-d-Y', strtotime($user->created_at)) }}
+                    <div class="flex flex-col items-end justify-between">
+                        <div>
+                            {{ __('Joined us ') . date('m-d-Y', strtotime($user->created_at)) }}
+                        </div>
+                        @if ($accountOwner)
+                        <a href="{{route('profile.edit')}}" class="text-sky-400">
+                            Account settings
+                        </a>
+                        @endif
                     </div>
                 </div>
                 @if ($user->description)
-                <div class="bg-gray-100 italic h-[4rem] px-4 py-2 mt-2">
-                    {{ __('Description (to do)') }}
-                </div>
+                    <div class="bg-gray-100 italic h-[4rem] px-4 py-2 mt-2 whitespace-pre-line">
+                        {{ $user->description }}
+                    </div>
                 @endif
             </div>
             <div x-data="{
-                selected: 'posts',
+                selected: null,
+                changeSelected(navItem) {
+                    this.selected = navItem
+                    sessionStorage.setItem('userProfileNavigation', navItem)
+                },
                 isSelected(navPart) {
                     if (this.selected == navPart) {
                         return true
                     } else {
                         return false
                     }
+                },
+                init() {
+                    if (sessionStorage.getItem('userProfileNavigation')) {
+                        this.selected = sessionStorage.getItem('userProfileNavigation')
+                    } else {
+                        this.selected = 'posts'
+                    }
                 }
             }" class="mt-4">
                 <div class="flex gap-4 tracking-wider px-4">
                     <div class="cursor-pointer pb-2 border-blue-300 hover:border-b-2 hover:border-gray-300"
                         x-bind:class="{ 'border-b-2 hover:border-blue-300': isSelected('posts') }"
-                        x-on:click="selected = 'posts'">
-                        {{ __('Posts') }}
+                        x-on:click="changeSelected('posts')">
+                        {{ __('Articles') }}
                     </div>
                     <div class="cursor-pointer border-blue-300 pb-2 hover:border-b-2 hover:border-gray-300"
                         x-bind:class="{ 'border-b-2 hover:border-blue-300': isSelected('comments') }"
-                        x-on:click="selected = 'comments'">
+                        x-on:click="changeSelected('comments')">
                         {{ __('Comments') }}
                     </div>
                 </div>
                 <div class="mt-4">
                     <template x-if="isSelected('posts')">
                         <div>
+                            @if (count($posts))
                             @foreach ($posts as $post)
                                 <x-user-profile.post-card :post="$post" />
                             @endforeach
+                            @else
+                                <div class="text-xs w-full text-center py-4">
+                                    {{__('This user hasn\'t published any articles yet.')}}
+                                </div>
+                            @endif
                         </div>
                     </template>
                     <template x-if="isSelected('comments')">
                         <div>
+                            @if (count($comments))
                             @foreach ($comments as $comment)
                                 <x-user-profile.comment-card :comment="$comment" />
                             @endforeach
+                            @else 
+                                <div class="text-xs w-full text-center py-4">
+                                    {{__('This user has no comments.')}}
+                                </div>
+                            @endif
                         </div>
                     </template>
                 </div>
